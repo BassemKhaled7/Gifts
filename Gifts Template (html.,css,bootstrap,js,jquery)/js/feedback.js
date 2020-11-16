77ce00465dbf45e3626190984124df971e361589
Bassem Khaled <54443087+BassemKhaled7@users.noreply.github.com>
2020-11-16 16:21:59 +0200

    Add files via upload

---
+++ Gifts Template (html.,css,bootstrap,js,jquery)/js/feedback.js
@@ -0,0 +1,46 @@
+
+function rateExp(){
+
+    $(".AHrateExpBtn").click (function(){
+
+        $(".AHrateExpBtn").css({'backgroundColor':'#fff'  , 'borderColor':'#cdcdcd' , 'color':'#593f3b'});
+        $(".AHitem i").css("color" , "#cdcdcd");
+        $(this).css({'backgroundColor':'#cdcdcd'  , 'borderColor':'black' ,'color':'black'});
+        $(this).siblings().css("color" , "black"); 
+    
+    }) ;
+    
+};
+
+function rateServe(){
+        
+    $(".AHrateServBtn").click(function(){
+
+        $(".AHrateServBtn").css({'backgroundColor':'#fff'  , 'color':'#593f3b', 'borderColor':'#cdcdcd'});
+        $(this).css({'backgroundColor':'#cdcdcd'  , 'color':'black', 'borderColor':'black'})
+    });
+};
+
+function rateShare(){  
+
+    $(".AHrateShareBtn").click(function(){
+
+        $(".AHrateShareBtn").css({'backgroundColor':'#fff'  , 'color':'#593f3b', 'borderColor':'#cdcdcd'});
+        $(this).css({'backgroundColor':'#cdcdcd'  , 'color':'black', 'borderColor':'black'})
+    });
+
+
+};
+
+function textIsEmpty(){
+
+    var emptyMessage = $("#AHmessage");
+    emptyMessage.html("")
+    console.log(emptyMessage.val().length)   
+
+}
+
+textIsEmpty();
+rateExp();
+rateServe();
+rateShare();
